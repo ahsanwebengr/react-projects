47beefa2561e693ace0c6d38b4e3f4551c0e5390
ahsanwebengr <ahsanwebengr@gmail.com>
2023-11-18 12:23:26 +0500

    complete backend API and Implement Frontent with Vite

--- blogs/server/index.js
+++ blogs/server/index.js
@@ -12,11 +12,25 @@ import authRoute from './routes/auth.js';
 import userRoute from './routes/users.js';
 import postRoute from './routes/posts.js';
 import categoryRoute from './routes/categories.js';
-import multer from 'multer'
+import multer from 'multer';
 import mongoose from "mongoose";
 
 mongoose.connect(str).then(console.log('DB is connected successfully')).
     catch(err => console.log(err));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name);
+    },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+});
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
