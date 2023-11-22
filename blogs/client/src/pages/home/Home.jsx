cda3bffe89f204194a1bec52a080d08727ee39ac
ahsanwebengr <ahsanwebengr@gmail.com>
2023-11-22 20:55:41 +0500

    calling API in cleint side

--- blogs/client/src/pages/home/Home.jsx
+++ blogs/client/src/pages/home/Home.jsx
@@ -3,9 +3,21 @@ import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
 import Sidebar from '../../components/sidebar/Sidebar';
 import "./home.css";
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
 const Home = () => {
     const location = useLocation();
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const resp = await axios.get('http://localhost:3000/api/posts');
+            console.log(resp);
+        };
+
+        fetchData();
+    }, []);
     return (
         <>
             <Header />
