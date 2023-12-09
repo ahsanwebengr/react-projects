fedf65958835fe5b535bb6b038327c2b30b83bef
ahsanwebengr <ahsanwebengr@gmail.com>
2023-12-09 14:27:56 +0500

    feat: pass data through link

--- next-app/app/project/page.jsx
+++ next-app/app/project/page.jsx
@@ -12,11 +12,12 @@ const Project = () => {
         <section className="container max-w-6xl mx-auto py-6">
             <h2 className="text-4xl font-semibold mb-4">Project Page</h2>
             <div className="grid grid-cols-3 gap-5">
-                {projects.map((project) => (
-                    <Link href={`project/${project.id}`} key={project?.id} className="flex flex-col gap-2 p-3 shadow-lg rounded-md hover:shadow-xl">
+                {projects?.map((project) => (
+                    <div key={project?.id} className="flex flex-col items-center justify-center gap-2 p-3 shadow-lg rounded-md h-40 hover:shadow-xl">
                         <h4 className="font-medium text-2xl">{project?.title}</h4>
                         <p>{project?.description}</p>
-                    </Link>
+                        <Link className="p-2 bg-gray-500 hover:bg-gray-800 px-5 text-white text-center rounded-md" href={{ pathname: `project/${project.id}`, query: { title: project.title, desc: project.description } }}>View</Link>
+                    </div>
                 ))}
             </div>
         </section>
