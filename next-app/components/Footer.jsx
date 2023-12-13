e9f2c4d39d63cfc8f01fed218a8d5195c8e0a2ef
ahsanwebengr <ahsanwebengr@gmail.com>
2023-12-13 09:38:49 +0500

    feat: API create

--- next-app/components/Footer.jsx
+++ next-app/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className='bg-black text-white text-center py-3'>
-            <p>© All right are reserved.</p>
+            <p>© {new Date().getFullYear()} All right are reserved.</p>
         </footer>
     );
 };
