1a5522c4b6e7357323c92efb4a4513a415d611d2
ahsanwebengr <ahsanwebengr@gmail.com>
2023-12-18 19:25:02 +0500

    tabs size adjust

--- next-app/app/products/layout.jsx
+++ next-app/app/products/layout.jsx
@@ -6,18 +6,12 @@ const layout = ({ children }) => {
     const pathname = usePathname();
     return (
         <>
-            <div className="tabs tabs-boxed">
+            <div className="tabs tabs-boxed mx-auto max-w-md">
                 <Link className={`${pathname === '/products/phone' ? 'tab-active tab' : 'tab'}`} href={'/products/phone'}>Phone</Link>
                 <Link className={`${pathname === '/products/phonecase' ? 'tab-active tab' : 'tab'}`} href={'/products/phonecase'}>Phone Case</Link>
                 <Link className={`${pathname === '/products/watch' ? 'tab-active tab' : 'tab'}`} href={'/products/watch'}>Watch</Link>
                 <Link className={`${pathname === '/products/accessories' ? 'tab-active tab' : 'tab'}`} href={'/products/accessories'}>Accessories</Link>
             </div>
-
-            {/* <div  className="">
-                <a  className="tab">Tab 1</a>
-                <a className="tab tab-active">Tab 2</a>
-                <a  className="tab">Tab 3</a>
-            </div> */}
 
             <section className="py-8">{children}</section>
         </>
