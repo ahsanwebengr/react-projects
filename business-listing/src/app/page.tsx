f8d551c7a642cbf984771e6b716193b532062f62
Muhammad Ahsan <92372278+ahsanwebengr@users.noreply.github.com>
2024-01-03 22:37:44 +0500

    Delete business-listing directory

--- business-listing/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image';
-
-export default function Home() {
-  return (
-    <main>
-      <h1>hello Next JS</h1>
-    </main>
-  );
-}
--- business-listing/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <footer className="p-20 bg-gray-100 flex items-center justify-center">
-            <p className="text-black text-lg">Developer By Muhammad Ahsan ©️ 2023</p>
-        </footer>
-    );
-};
-
-export default Footer;
