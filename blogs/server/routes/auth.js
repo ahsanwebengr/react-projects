bb0a722a2cad3be49e94a09e92373e44d9561410
Muhammad Ahsan <92372278+ahsanwebengr@users.noreply.github.com>
2024-01-29 23:20:11 +0500

    Update auth.js

--- blogs/server/routes/auth.js
+++ blogs/server/routes/auth.js
@@ -26,10 +26,10 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
     try {
         const user = await User.findOne({ username: req.body.username });
-        !user && res.status(400).json('Wrong Credentials');
+        !user && res.status(400).json('Wrong Credentials!');
 
         const validated = await bcrypt.compare(req.body.password, user.password);
-        !validated && res.status(400).json('Wrong Password');
+        !validated && res.status(400).json('Wrong Password!');
 
         const { password, ...others } = user._doc;
         res.status(200).json(others);
