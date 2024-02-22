3c55991facd2ebec8bd7fd8dfbfe1bfc5ed5041e
ahsanwebengr <ahsanwebengr@gmail.com>
2024-02-22 09:47:17 +0500

    install: tailwind css

--- rtk-query/src/components/Recipe.jsx
+++ rtk-query/src/components/Recipe.jsx
@@ -6,11 +6,11 @@ const MyComponent = () => {
     console.log(data, '->API DATA');
 
     return (
-        <div>
+        <div className=''>
             {data && data?.recipes?.map(recipe => (
                 <article key={recipe?.id}>
-                    <img src={recipe?.image} alt={recipe?.name} />
-                    <div>{recipe?.tags && recipe.tags?.map(tag => (
+                    <img className='w-20' src={recipe?.image} alt={recipe?.name} />
+                    <div key={recipe?.id}>{recipe?.tags && recipe.tags?.map(tag => (
                         <span>{tag}</span>
                     ))}</div>
                     <h4>{recipe?.name}</h4>
