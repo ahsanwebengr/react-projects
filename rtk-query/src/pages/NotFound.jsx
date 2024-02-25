7eccceb002b6ded08565879283d16e7e333b2d39
ahsanwebengr <ahsanwebengr@gmail.com>
2024-02-25 17:54:14 +0500

    fix: Not Found Page

--- rtk-query/src/pages/NotFound.jsx
+++ rtk-query/src/pages/NotFound.jsx
@@ -1,11 +1,13 @@
+import { Home } from 'lucide-react';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
         <div className="min-h-screen flex justify-center items-center text-center flex-col gap-4">
+            <h1 className='text-5xl font-bold tracking-widest dark:text-white mb-2'>404</h1>
             <h1 className='text-4xl font-semibold dark:text-white'>Page Not Found...!</h1>
-            <Link to={'/'} className='btn btn-primary'>Back to Home</Link>
+            <Link to={'/'} className='btn btn-circle btn-primary'><Home /></Link>
         </div>
     );
 };
